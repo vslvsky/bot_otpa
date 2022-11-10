aaab75874b7d5e53d28ffbe925149748233a0401
Никита Василевский <n.vasilevsky@sibset-office.211.ru>
2022-11-10 17:58:24 +0700

    update second chart and delete dirt

--- otpa_infobot.py
+++ otpa_infobot.py
@@ -1,44 +1,19 @@
-import requests
 import datetime
-import numpy
-import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import DataBase as dbase
 import VitalityBooster as vb
 import warnings
-from prettytable import PrettyTable
 import seaborn as sns
 import matplotlib.pyplot as plt
 import telebot
+
 from my_token import token
+from my_token import chat_id
+
 
 warnings.filterwarnings('ignore')
+bot = telebot.TeleBot(token)
 
-url = f"https://api.telegram.org/bot{token}/sendMessage"
-
-def get_datatime() -> datetime:
-    date_format = '%H:%M:%S %Y.%m.%d'
-    today = datetime.now() #- timedelta(days=1)
-    today = today.strftime(date_format)
-    return today
-
-
-def send_message(text: str, chat_id: str, url: str) -> None:
-    params = {'text': text,
-              'chat_id': chat_id}
-    requests.post(url=url, params=params)
-
-def send_photo(photo, chat_id: str, url: str) -> None:
-    params = {'photo': photo,
-              'chat_id': chat_id}
-    requests.post(url=url, params=params)
-
-
-def get_dataframe(select_sql) -> dict:
-    dl = vb.MessengerSQL(dbase.PostgreSQL_Datalake())
-    dl.connect()
-    dataframe = dl.send_command(select_sql)
-    return dataframe
 
 select_1hours = """select  closing_user,
         count(terrasoft_number)
@@ -120,69 +95,63 @@ select_today = """select  closing_user,
 		order by 2 desc"""
 
 
-
-dataframe_1hour = get_dataframe(select_1hours)
-header = ['closing_user', 'count']
-table_closing_1hour = PrettyTable(vertical_char='-', junction_char='-', header=False, title='Кол-во закрытых за час')
-table_closing_1hour.add_column("closing_user", dataframe_1hour['closing_user'].to_list())
-table_closing_1hour.add_column("count", dataframe_1hour['count'].to_list())
-
-dataframe_today = get_dataframe(select_today)
-header = ['closing_user', 'count']
-table_closing_today = PrettyTable(vertical_char='-', junction_char='-', header=False, title='Кол-во закрытых за сегодня')
-table_closing_today.add_column("closing_user", dataframe_today['closing_user'].to_list())
-table_closing_today.add_column("count", dataframe_today['count'].to_list())
+def get_datatime() -> datetime:
+    date_format = '%H:%M:%S %d.%m.%Y'
+    today = datetime.now()
+    today = today.strftime(date_format)
+    return today
 
 
-# Some boilerplate to initialise things
-sns.set()
-plt.figure()
-
-# This is where the actual plot gets made
-ax = sns.barplot(data=dataframe_today, y="closing_user", x="count", saturation=0.6)
-
-
-cities = ['Paris','Moscow','Rome','London']
-values = [175,380,445,470]
-
-#bars = plt.bar(cities, values)
-#ax = plt.gca()
-
-# Customise some display properties
-ax.set_title('Закрытые инциденты за сегодня')
-ax.grid(color='#cccccc')
-ax.set_ylabel('Количество закрытых инцидентов')
-ax.set_xlabel(None)
-#ax.set_yticklabels(dataframe_today["closing_user"], figsize=(3,3))
-#ax.text("test")
+def get_dataframe(select_sql) -> dict:
+    dl = vb.MessengerSQL(dbase.PostgreSQL_Datalake())
+    dl.connect()
+    dataframe = dl.send_command(select_sql)
+    return dataframe
 
 
-#ax.set_xticklabels(dataframe_today["count"].unique().astype(str), rotation='horizontal')
-# Ask Matplotlib to show it
-#plt.show()
-plt.tight_layout()
+def do_chart_today():
+    dataframe_today = get_dataframe(select_today)
+    color = sns.color_palette('Reds_r', 25)
 
-plt.savefig('foo1.png')
+    sns.set()
+    plt.figure()
+
+    ax = sns.barplot(data=dataframe_today, y="closing_user", x="count", palette=color)
+    ax.set_title("Закрытые инциденты за сегодня")
+    ax.grid(color='#cccccc')
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    plt.tight_layout()
+    plt.savefig('closing_today.png')
 
 
-url_photo = f"https://api.telegram.org/bot{token}/sendPhoto"
+def do_chart_1hour():
+    dataframe_1hour = get_dataframe(select_1hours)
+    color = sns.color_palette('Reds_r', 15)
 
+    sns.set()
+    plt.figure()
 
-text = f" \n На {get_datatime()} \n {table_closing_1hour} \n\n {table_closing_today}"
+    ax = sns.barplot(data=dataframe_1hour, y="closing_user", x="count", palette=color)
+    ax.set_title('Закрытые инциденты за 1 час')
+    ax.grid(color='#cccccc')
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    plt.tight_layout()
+    plt.savefig('closing_1hour.png')
 
-#chat_id = '@otpa_infobot'
-chat_id = '1105837299'
-
-bot = telebot.TeleBot(token)
-
-photo = open('foo1.png', 'rb')
-bot.send_photo(chat_id, photo)
-bot.send_message(chat_id, 'test telebot')
 
 def main():
-    send_message(text, chat_id, url)
 
+    do_chart_today()
+    do_chart_1hour()
 
+    photo_today = open('closing_today.png', 'rb')
+    photo_1hour = open('closing_1hour.png', 'rb')
+
+    bot.send_message(chat_id, f"Информация на {get_datatime()}")
+    bot.send_photo(chat_id, photo_today)
+    bot.send_photo(chat_id, photo_1hour)
 
 if __name__ == '__main__':
     main()
